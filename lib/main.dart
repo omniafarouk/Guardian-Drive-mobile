0d04ee3aca0f6f55f8c987ccaff530d59cf1b4db
Mayar-ashraf <mayarashraf2002@gmail.com>
2026-05-24 04:14:52 +0300

    fix(main) : fixed alert list route

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:app_links/app_links.dart';
 import 'package:guardian_drive_mobile/pages/alert_list_page.dart';
 import 'package:guardian_drive_mobile/pages/dashboard.dart';
 import 'package:guardian_drive_mobile/pages/login_page.dart';
-import 'package:guardian_drive_mobile/pages/ongoing_trip_page.dart';
 import 'package:guardian_drive_mobile/pages/profile_screen.dart';
 import 'package:guardian_drive_mobile/pages/trip_details_page.dart';
 import 'package:guardian_drive_mobile/pages/trip_list_page.dart';
@@ -120,7 +119,7 @@ class _MyAppState extends State<MyApp> {
         '/home': (context) => Dashboard(),
         '/profile': (context) => const ProfileScreen(),
         '/trips': (context) => const tripListPage(),
-        '/alerts': (context) => const OngoingTripPage(),
+        '/alerts': (context) => const AlertListPage(),
         '/login': (context) => LoginPage(),
         '/alert-details': (context) => AlertDetail(),
         '/trip-details': (context) => TripDetailsPage(),
