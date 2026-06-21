891df9bf68cbc7223ebb68137b468ec3770c9149
Omnia Farouk <omniafarouk2003@gmail.com>
2026-06-21 17:08:42 +0300

    deleted failed files

--- lib/pages/alert_list_page.dart
+++ lib/pages/alert_list_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:guardian_drive_mobile/models/alert_summary.dart';
-import 'package:guardian_drive_mobile/models/location_coords.dart';
 import 'package:guardian_drive_mobile/services/alert_service.dart';
 import 'package:guardian_drive_mobile/widgets/background.dart';
 import 'package:guardian_drive_mobile/widgets/custom_app_bar.dart';
@@ -8,7 +7,6 @@ import 'package:guardian_drive_mobile/widgets/alert_list_item.dart';
 import 'package:guardian_drive_mobile/widgets/filter_alert.dart';
 import 'package:guardian_drive_mobile/widgets/side_bar_drawer.dart';
 import 'package:number_paginator/number_paginator.dart';
-
 
 class AlertListPage extends StatefulWidget {
   const AlertListPage({super.key});
